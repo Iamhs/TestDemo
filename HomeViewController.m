efab81dc92b7171894c3a0faaf5e26f58dabb4de
Iamhs <hs5526373@163.com>
2016-09-27 00:28:51 +0800

    no message

--- HomeViewController.m
+++ HomeViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //alksjdflkjaslkdfjl askdjfl kajsdflk jaslkd fjlk asdf
 }
 
 - (void)didReceiveMemoryWarning {
