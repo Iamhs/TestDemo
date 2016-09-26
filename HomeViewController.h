2799a1226861ca85977b1e8052b673b504d7de90
Iamhs <hs5526373@163.com>
2016-09-27 00:24:31 +0800

    no message

---
+++ HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  SCLanguage
+//
+//  Created by iamdzz on 16/7/17.
+//  Copyright © 2016年 com.hs.123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SCLanguage
+//
+//  Created by iamdzz on 16/7/17.
+//  Copyright © 2016年 com.hs.123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
